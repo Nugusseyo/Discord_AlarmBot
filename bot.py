bb9d6f125782c48462414e3adaa812a2bbf40206
Nugusaeyo <chsj0702@gmail.com>
2026-05-09 14:39:31 +0900

    Uptime robot

--- bot.py
+++ bot.py
@@ -425,3 +425,19 @@ async def on_ready():
 
 import os
 bot.run(os.environ["DISCORD_TOKEN"])
+
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive"
+
+def run_web():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    t = Thread(target=run_web)
+    t.start()
